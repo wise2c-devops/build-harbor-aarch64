a42e8c49f11fb9de3e044cae77ebf4ce12d28084
Alan Peng <peng.alan@gmail.com>
2023-11-13 09:39:37 -0500

    replace make/photon/Makefile

--- build-harbor-aarch64.sh
+++ build-harbor-aarch64.sh
@@ -13,10 +13,12 @@ git clone --branch ${GIT_BRANCH} https://github.com/goharbor/harbor.git src/gith
 mkdir v2.7.2
 cd v2.7.2
 git clone --branch v2.7.2 https://github.com/goharbor/harbor.git src/github.com/goharbor/harbor
-#rm -f ../harbor/Makefile
-rm -f ../harbor/make/photon/Makefile
-#cp harbor/Makefile ../harbor/Makefile
-cp harbor/make/photon/Makefile ../harbor/make/photon/Makefile
+cd harbor
+#rm -f ../../harbor/Makefile
+rm -f ../../harbor/make/photon/Makefile
+#cp Makefile ../../harbor/Makefile
+cp make/photon/Makefile ../../harbor/make/photon/Makefile
+cd ../../
 
 # Fix the issue of "missing separator (did you mean TAB instead of 8 spaces?)"
 #sed -i 's/^[[:space:]]*/	/g' harbor/Makefile
