22b46b31142bae2527e6a4b41756a010670a7783
Alan Peng <peng.alan@gmail.com>
2024-01-13 22:19:52 +0800

    Update build-harbor-aarch64.sh

--- build-harbor-aarch64.sh
+++ build-harbor-aarch64.sh
@@ -23,4 +23,4 @@ make pre_update
 make compile COMPILETAG=compile_golangimage
 
 # Build harbor arm image:
-make build GOBUILDTAGS="include_oss include_gcs" BUILDBIN=true NOTARYFLAG=true TRIVYFLAG=true CHARTFLAG=true GEN_TLS=true PULL_BASE_FROM_DOCKERHUB=false
+make build GOBUILDTAGS="include_oss include_gcs" BUILDBIN=true NOTARYFLAG=false TRIVYFLAG=true CHARTFLAG=false GEN_TLS=true PULL_BASE_FROM_DOCKERHUB=true
